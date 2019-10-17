d8c788b1e32035108a25307932cf4bffe11b71ae
gokcenturkoz <gokcenturkoz@anadolu.edu.tr>
2019-10-17 16:26:16 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+    fs.readFile('index.html',function(err,data){
+        res.write(data);
+        res.end('end');
+    
+    });
+    
+});
+server.listen(8000);
